c75e6956f8f8e808688cd2c38d33520300dc45aa
Emily Ochoa <eochoa156@gmail.com>
2024-07-01 22:06:11 -0700

    mult_list function done

---
+++ main.py
@@ -0,0 +1,17 @@
+def max_num(a,b,c):
+    return max([a,b,c])
+
+def mult_list(lst):
+  if len(lst) == 0:
+    return 0
+  prod = lst[0]
+  if len(lst) > 1:
+    for i in lst[1:]:
+      prod = prod * i
+  return prod
+
+def rev_string():
+    return ()
+
+def num_within():
+    return ()
